e4a8f5e7677c2cec262a3ed98edba18adfd55d93
ymgtech <ymgtech@millenary.net>
2018-08-26 16:08:58 +0900

    [add] DjangoでMySQLを使えるよう設定

--- lurcher/settings/development.py
+++ lurcher/settings/development.py
@@ -1,1 +1,13 @@
 from .base import *
+
+# この設定でMySQLの構成お願いします
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lurcher',
+        'USER': 'lurcher',
+        'PASSWORD': 'lurcher',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
