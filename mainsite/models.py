aceaf55a6c6759e1c587c0333e490121a192e990
ymgtech <ymgtech@millenary.net>
2018-08-27 12:52:29 +0900

    [add] 管理画面でテストデータを挿入できるよう設定

--- mainsite/models.py
+++ mainsite/models.py
@@ -10,15 +10,15 @@ class Account(models.Model) :
     held_count = PositiveBigIntegerField('開催数', default = 0)
 
     def __str__(self) :
-        return self.twitter_id
+        return str(self.twitter_id)
 
 class OfflineParty(models.Model) :
     sponsor = models.ForeignKey(Account, on_delete = models.CASCADE)
     title = models.CharField('タイトル', max_length = 255)
-    at_time = models.DateTimeField('開催日時', null = True)
+    at_time = models.DateTimeField('開催日時', null = True, blank=True)
     capacity = models.IntegerField('募集人数')
-    location_lat = models.FloatField('緯度', null = True)
-    location_lng = models.FloatField('経度', null = True)
+    location_lat = models.FloatField('緯度', null = True, blank=True)
+    location_lng = models.FloatField('経度', null = True, blank=True)
     recruitment_start = models.DateField('募集開始')
     recruitment_end = models.DateField('募集終了')
     comment = models.TextField('コメント')
