e1d5e9937f6853a1fe1fcdba687f3a755bf2c0bb
ymgtech <ymgtech@millenary.net>
2018-08-27 16:41:18 +0900

    [add] マイグレーションを作成、管理画面でテストデータを挿入できるよう設定

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Account, OfflineParty
+from .models import Account, AccountCategory, OfflineParty
 
 # Register your models here.
 admin.site.register(Account)
+admin.site.register(AccountCategory)
 admin.site.register(OfflineParty)
