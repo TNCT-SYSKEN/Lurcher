414aa8f59ab4fa5709dd46d078dd76ade5227d7d
Kaito Sato <ksato.nit@gmail.com>
2018-08-28 13:32:56 +0900

    [add]詳細ページの動的生成をちょっと書いた

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from mainsite.models import OfflineParty
 
 # Create your views here.
 
 def details(request):
-	return render(request, 'mainsite/details.html')
+	party = OfflineParty.objects.get(id=1)
+	template = loader.get_template('mainsite/details.html')
+	context = {
+		'party': party,
+	}
+	return HttpResponse(template.render(context, request))
