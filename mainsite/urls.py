023f01fc33022405ee376ccc7487b49a2920ccbf
shimesaba0218 <shimesaba0218@gmail.com>
2018-08-29 11:31:41 +0900

    [fix]ログインして/にアクセスするとエラーになる問題の解決

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 import django.contrib.auth.views as auth_views
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(), {'template_name': 'mainsite/login.html'}, name='login'),
     path('top/',views.top_page, name="top"), # リダイレクト
-    path('login/', auth_views.LoginView.as_view(), {'template_name': 'mainsite/login.html'}, name='login'),
     path('signup/', views.signup, name="signup"),
 ]
