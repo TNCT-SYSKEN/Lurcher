ac8645439cd65b47359be8f8d6f7ce4c23238d43
Kaito Sato <ksato.nit@gmail.com>
2018-08-29 19:59:35 +0900

    [add] カテゴリと参加者を動的に取得

--- mainsite/views.py
+++ mainsite/views.py
@@ -6,9 +6,22 @@ from mainsite.models import OfflineParty
 # Create your views here.
 
 def details(request):
-	party = OfflineParty.objects.get(id=1)
+	party = OfflineParty.objects.get(id=2)
+	categories = party.category.all()
+	categories_str = ""
+	for category in categories:
+		categories_str += str(category) + " "
+
+	participants = party.participant.all()
+	participants_str = ""
+	for participant in participants:
+		participants_str += str(participant) + " "
+		if len(participants_str) >= 15: #マジックナンバーをなんとかしたい
+			break
 	template = loader.get_template('mainsite/details.html')
 	context = {
 		'party': party,
+		'categories': categories_str,
+		'participants': participants_str
 	}
 	return HttpResponse(template.render(context, request))
