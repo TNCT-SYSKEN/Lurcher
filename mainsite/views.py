7dd94088fdd281c1bb5537476bb84d2a4d71d24d
Kaito Sato <ksato.nit@gmail.com>
2018-08-29 20:37:33 +0900

    [change] Twitter API を利用したユーザ名の取得

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,26 +2,38 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from mainsite.models import OfflineParty
+import tweepy
 
 # Create your views here.
 
 def details(request):
 	party = OfflineParty.objects.get(id=2)
+
+	auth = tweepy.OAuthHandler('Consumer Token', 'Consumer Secret')
+	auth.set_access_token('Access Token', 'Access Secret')
+	api = tweepy.API(auth)
+
+	sponsor_account = api.get_user(user_id=party.sponsor.twitter_id)
+	sponsor_name = sponsor_account.screen_name
+
 	categories = party.category.all()
 	categories_str = ""
+
 	for category in categories:
 		categories_str += str(category) + " "
 
 	participants = party.participant.all()
 	participants_str = ""
 	for participant in participants:
-		participants_str += str(participant) + " "
+		participants_str += api.get_user(user_id=participant.twitter_id).screen_name + " "
 		if len(participants_str) >= 15: #マジックナンバーをなんとかしたい
 			break
+
 	template = loader.get_template('mainsite/details.html')
 	context = {
 		'party': party,
 		'categories': categories_str,
-		'participants': participants_str
+		'participants': participants_str,
+		'sponsor_name': sponsor_name,
 	}
 	return HttpResponse(template.render(context, request))
