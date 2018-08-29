f839f71274365a365a6aa228ac9058200c2ebcac
ymgtech <ymgtech@millenary.net>
2018-08-29 22:22:13 +0900

    [fix] 誤ってプッシュされた競合の修正

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,8 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def create(request):
-<<<<<<< HEAD
-    return render(request, 'mainsite/asobi.html')
-=======
     return render(request, 'mainsite/add-settings.html')
->>>>>>> 00ab3d29ff652b736dbab6287b2d6c69ce566225
