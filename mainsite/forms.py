26ee4f40e25cc48073dffa644214bdfb5f44dca5
ymgtech <ymgtech@millenary.net>
2018-08-29 21:57:20 +0900

    [add] フォームの内容をカスタムフォームに変更

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from .models import OfflineParty
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class PageCreateForm(forms.ModelForm):
     class Meta:
         model = OfflineParty
-        fields = ['title','at_time','capacity', 'recruitment_start','recruitment_end', 'comment']
+        fields = ['title', 'capacity', 'recruitment_start','recruitment_end', 'comment']
+        widgets = {
+            'recruitment_start': DateInput(),
+            'recruitment_end': DateInput()
+        }
 
     def __init__(self, *args, **kwargs) :
         super().__init__(*args, **kwargs)
