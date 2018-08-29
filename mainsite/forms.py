1a7d4494251f58acd9f3c1ac515b4723e162014e
ymgtech <ymgtech@millenary.net>
2018-08-29 21:44:12 +0900

    [add] デザインを作成

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -7,3 +7,8 @@ class PageCreateForm(forms.ModelForm):
         fields = ['title','at_time','capacity','location_lat',
         'location_lng','recruitment_start','recruitment_end',
         'comment']
+
+    def __init__(self, *args, **kwargs) :
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
