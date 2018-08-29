a612746077e7a394a4857b1038c6dafeaf83fe2c
ymgtech <ymgtech@millenary.net>
2018-08-29 18:44:03 +0900

    [fix] 使われていないビューを削除

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,10 +4,8 @@ from . import views
 app_name = 'lurcher'
 
 urlpatterns = [
-    path('create', views.create, name = 'create'),
-    #createは使ってません
     path('offcreate',views.add, name='add'),
     path('home/',views.home, name='return_to_home')
     #home/でviewsの中のhomeという関数を呼び出し、
     #ホームのページに戻そうと思っているので調整をお願いします
-]]
+]
