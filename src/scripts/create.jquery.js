5bafed449d0a0ff91e762389ccaa4392031531bd
ymgtech <ymgtech@millenary.net>
2018-08-31 00:10:55 +0900

    [add] API経由でカテゴリを登録する機能を追加

--- src/scripts/create.jquery.js
+++ src/scripts/create.jquery.js
@@ -1,6 +1,7 @@
 $(function() {
     $("select").selectpicker()
 
+    /* 新規カテゴリ作成でセレクトに追加 */
     $('#add-new-category').on('click', function() {
         let category = $('input[name="input-new-category"]').val()
 
@@ -10,6 +11,20 @@ $(function() {
                 $('input[name="input-new-category"]').val('')
                 return $('<option>').val($('#id_category option').length + 1).text(category)
             }
+        })
+
+        /* APIでカテゴリを追加 */
+        $.ajax({
+            url: '/api/categories/',
+            type: 'POST',
+            headers: {
+                'X-CSRFTOKEN': Cookies.get('csrftoken'),
+            },
+            data: {
+                'category': category
+            }
+        }).fail(response => {
+            alert('エラーが発生しました')
         })
 
         $("select").selectpicker('refresh')
