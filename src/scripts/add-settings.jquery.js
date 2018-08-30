7158fd04078800f0adb3d4f4cd116cca66159f5f
ymgtech <ymgtech@millenary.net>
2018-08-30 15:46:19 +0900

    [add] 適当なコメントアウトを追加

--- src/scripts/add-settings.jquery.js
+++ src/scripts/add-settings.jquery.js
@@ -2,6 +2,7 @@ $(function() {
     let date, time
 
     /** マップ **/
+    /* 確定ボタンを押すと緯度・経度を非表示インプットに挿入 */
     $(document).on('click', '#set-latlng', function() {
         let latlng = marker.getLatLng()
         $('input:hidden[name="location_lat"]').val(latlng.lat)
@@ -10,6 +11,7 @@ $(function() {
     })
 
     /** 開催日時 **/
+    /* 日付と時間を結合して非表示インプットに挿入(JST) */
     $('input[name="hold-date"]').on('change', function() {
         date = $('input[name="hold-date"]').val()
         setAtTime()
