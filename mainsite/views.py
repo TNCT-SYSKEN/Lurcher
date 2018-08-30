af7cf6cb5602cdef74472d212ee7dccf09309a6c
shimesaba0218 <shimesaba0218@gmail.com>
2018-08-30 23:05:27 +0900

    [add]signupページをとりあえずで実装

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from mainsite.models import Account, OfflineParty
+from mainsite.models import Account, OfflineParty, Category
 from social_django.models import UserSocialAuth
+from .forms import AccountForm
 
 # Create your views here.
 def login(request) :
@@ -22,10 +23,20 @@ def top_page(request):
         return HttpResponse(template.render(context, request))
     else: # ここもうちょっとマシな実装したい
         return redirect('/signup/')
-def signup(request) : # 現在特に役割がない
-    user_id = request.user.id
+def signup(request) :
+    user = UserSocialAuth.objects.get(user_id=request.user.id)
+    form = AccountForm(request.POST or None, initial={"twitter_id":user.access_token["user_id"]})
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return render(request,'mainsite/top.html')
+
+    category = list()
+    for c in Category.objects.all():
+        category.append(str(c))
     template = loader.get_template('mainsite/signup.html')
     context = {
-        'user_id': user_id,
+        'form': form,
+        'category': category,
+        'user': user,
     }
     return HttpResponse(template.render(context, request))
