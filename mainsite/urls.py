4525b12b20862a540b12bb9acfeaf64a4c533895
ymgtech <ymgtech@millenary.net>
2018-08-30 15:11:26 +0900

    [add] カテゴリを取得するAPIを一気に実装

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryList)
+
 urlpatterns = [
+	path('api/', include(router.urls)),
 	path('details', views.details, name='details')
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from mainsite.models import OfflineParty
+from rest_framework import viewsets, serializers
+from django_filters import rest_framework as filters
+from mainsite.models import Category, OfflineParty
 
 # Create your views here.
 
 def details(request):
-	party = OfflineParty.objects.get(id=1)
-	template = loader.get_template('mainsite/details.html')
-	context = {
-		'party': party,
-	}
-	return HttpResponse(template.render(context, request))
+    party = OfflineParty.objects.get(id=1)
+    template = loader.get_template('mainsite/details.html')
+    context = {
+        'party': party,
+    }
+    return HttpResponse(template.render(context, request))
+
+class CategoryFilter(filters.FilterSet):
+    category = filters.CharFilter(lookup_expr='contains')
+    class Meta :
+        model = Category
+        fields = ['category']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class CategoryList(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [filters.DjangoFilterBackend,]
+    filter_class = CategoryFilter
