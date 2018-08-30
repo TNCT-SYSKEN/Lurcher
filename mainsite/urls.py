a5da20dbe2efdf41226a83357a3317d08d90837c
ymgtech <ymgtech@millenary.net>
2018-08-30 15:36:38 +0900

    [fix] コンフリクト解決時の誤りを修正

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -8,5 +8,5 @@ router.register(r'categories', views.CategoryList)
 urlpatterns = [
   path('api/', include(router.urls)),
 	path('details/<int:offline_party_id>', views.details, name='details'),
-	path('register/<int:offline_party_id>', views.register, name='register',
+	path('register/<int:offline_party_id>', views.register, name='register'),
 ]
