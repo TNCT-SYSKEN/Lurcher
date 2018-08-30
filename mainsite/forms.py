3a9e707342a0847fe6f4d078e9807042f861c86c
k.sato <ksato.nit@gmail.com>
2018-08-31 02:07:51 +0900

    [fix] さまざまな内容を含む

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Account
+from .models import Account, OfflineParty
 
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ['twitter_id', 'pref', 'category']
+        fields = ['twitter_id', 'pref', 'category', 'access_token', 'access_token_secret']
+
+class PageCreateForm(forms.ModelForm):
+    class Meta:
+        model = OfflineParty
+        fields = ['title','at_time','capacity','location_lat',
+        'location_lng','recruitment_start','recruitment_end',
+        'comment']
