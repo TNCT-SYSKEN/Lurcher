debdb626b046e8c728dac1cac7d775ca3c835658
ymgtech <ymgtech@millenary.net>
2018-08-30 23:11:35 +0900

    [add] 新規カテゴリを入力するとセレクトのオプションに追加するよう設定

--- src/scripts/create.jquery.js
+++ src/scripts/create.jquery.js
@@ -1,3 +1,17 @@
 $(function() {
-    $("select").selectpicker();
+    $("select").selectpicker()
+
+    $('#add-new-category').on('click', function() {
+        let category = $('input[name="input-new-category"]').val()
+
+        if(category === '') return
+        $('#id_category').append(function() {
+            if($('#id_category option:contains("' + category + '")').length === 0) {
+                $('input[name="input-new-category"]').val('')
+                return $('<option>').val($('#id_category option').length + 1).text(category)
+            }
+        })
+
+        $("select").selectpicker('refresh')
+    })
 })
