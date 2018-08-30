43b36f6648d49066c16b718fc5c35fd0ab7919a5
ymgtech <ymgtech@millenary.net>
2018-08-30 11:31:36 +0900

    [add] 開催時刻を送信できるよう設定

--- src/scripts/add-settings.jquery.js
+++ src/scripts/add-settings.jquery.js
@@ -1,7 +1,25 @@
 $(function() {
+    let date, time
+
+    /** マップ **/
     $(document).on('click', '#set-latlng', function() {
         let latlng = marker.getLatLng()
         $('input:hidden[name="location_lat"]').val(latlng.lat)
         $('input:hidden[name="location_lng"]').val(latlng.lng)
     })
+
+    /** 開催日時 **/
+    $('input[name="hold-date"]').on('change', function() {
+        date = $('input[name="hold-date"]').val()
+        setAtTime()
+    })
+    $('input[name="hold-time"]').on('change', function() {
+        time = $('input[name="hold-time"]').val()
+        setAtTime()
+    })
+
+    function setAtTime() {
+        let datetime = date + ' ' + time
+        $('input:hidden[name="at_time"]').val(datetime)
+    }
 })
