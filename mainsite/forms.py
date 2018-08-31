bf31b2614cb19b17c33d2ccfbebda1fa09992514
ymgtech <ymgtech@millenary.net>
2018-08-31 11:07:42 +0900

    [fix] フォームへのクラス指定が消えていた問題を修正

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -17,3 +17,13 @@ class PageCreateForm(forms.ModelForm):
         fields = ['title','at_time','capacity','location_lat',
         'location_lng','category','recruitment_start','recruitment_end',
         'comment']
+
+    def __init__(self, *args, **kwargs) :
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+        attrs = {
+            'data-live-search': 'true',
+        }
+        self.fields['category'].widget.attrs.update(attrs)
